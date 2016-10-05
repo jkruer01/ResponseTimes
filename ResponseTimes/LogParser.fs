d4f039fb97ab8e01ae8f078c7b6adcfe5f0ebb47
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-10-05 16:53:08 -0400

    Initial Commit

---
+++ ResponseTimes/LogParser.fs
@@ -0,0 +1,58 @@
+﻿module LogParser
+
+open System
+open System.IO
+open FSharp.Data
+open FSharp.Text.RegexProvider
+
+type Logs = CsvProvider<"logs.csv"> 
+type Details = JsonProvider<"details.json", SampleIsList=true>
+type ResponseTimeRegex = Regex< @"(?<=Request Finished in )((?<ResponseTime>\d+)(?= milliseconds))", noMethodPrefix = true >
+
+type LogDetails = {
+    ApplicationName : string;
+    LoggerName : string;
+    RequestedURL : Uri;
+    HttpResponseStatusCode : int;
+    HttpVerb : string;
+    Message : string;
+    TransactionId : Option<Guid>;
+    ResponseTime : Option<int>;
+}
+
+let createDetailsJson () =
+    let logs = Logs.Load("logs.csv")
+    use outFile = new StreamWriter("details.json")
+
+    outFile.WriteLine(sprintf "%s" "[")
+
+    let details = 
+        logs.Rows 
+        |> Seq.take(100000)
+        |> Seq.map(fun log -> log.Details)
+        |> Seq.iter (fun detail -> outFile.WriteLine(sprintf "%s," detail))
+
+
+    outFile.WriteLine(sprintf "%s" "]")
+    outFile.Close ()
+
+let convertLogToLogDetails (log : Logs.Row) =
+    let details = Details.Parse(log.Details)
+    let responseTime = ResponseTimeRegex().Match(details.Message).ResponseTime.Value
+    {
+        ApplicationName = log.ApplicationName;
+        LoggerName = log.LoggerName;
+        RequestedURL = new Uri(details.RequestedUrl);
+        HttpResponseStatusCode = details.HttpResponseStatusCode;
+        HttpVerb = details.HttpVerb;
+        Message = details.Message;
+        TransactionId = details.TransactionId;
+        ResponseTime = match responseTime with
+                       | "" -> None
+                       | _ -> Some (Int32.Parse(responseTime))
+    }
+
+
+let parseCsv (fileName : string) =
+    Logs.Load(fileName).Rows
+    |> Seq.map(fun log -> convertLogToLogDetails log)
---
+++ ResponseTimes/Program.fs
@@ -0,0 +1,51 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open LogParser
+open System
+open System.IO
+
+let removeEverythingAfterLast (separator : char) (input : string) =
+    let parts = input.Split([|separator|], StringSplitOptions.RemoveEmptyEntries)
+    let numberOfItems = parts.Length 
+    let strSeparator = separator.ToString()
+
+    Seq.take (numberOfItems - 1) parts
+    |> Seq.toList
+    |> String.concat strSeparator
+    |> (fun result -> strSeparator + result)
+
+let cleanseRequestedUrl (url : string) =
+    let lower = url.ToLower()
+                    .Replace("/sams", "")
+                    .Replace("/service", "")
+                    .Replace("/ess", "")
+
+    if (lower.Contains "api/v1/documents/" || lower.Contains "api/v1/eftaccounts/")  then
+        removeEverythingAfterLast '/' lower
+    else lower
+
+let averageResponseTimesByRequestedURL logDetails =
+    Seq.filter(fun logDetail -> logDetail.ResponseTime.IsSome && logDetail.ResponseTime.Value > 0) logDetails
+    |> Seq.groupBy(fun logDetail -> sprintf "%s,%s" logDetail.HttpVerb (cleanseRequestedUrl logDetail.RequestedURL.AbsolutePath)) 
+    |> Seq.map(fun (key, group) -> key, group |> Seq.map(fun logDetail -> float logDetail.ResponseTime.Value) |> Seq.average)
+    |> Seq.sortByDescending(fun (key, value) -> value)
+
+
+
+
+[<EntryPoint>]
+let main argv = 
+    let outputFile = "results.csv"
+    use sw = new StreamWriter(outputFile)
+    fprintfn sw "Method,Url,Avg ResposneTime (ms)"
+
+    LogParser.parseCsv "logs.csv"
+    |> averageResponseTimesByRequestedURL
+    |> Seq.iter(fun (url, avgResponseTime) -> fprintfn sw "%s,%.1f" url avgResponseTime)
+
+    sw.Close();
+
+    printfn "Results saved to %s" outputFile
+    printfn "Press any key to continue..."
+    let input = Console.ReadLine()
+    0 // return an integer exit code
